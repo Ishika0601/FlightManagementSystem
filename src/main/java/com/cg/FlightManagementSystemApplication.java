845e425971f97d9c2f7f22c1774121f4e9b3098d
devampire11 <hy3588211@gmail.com>
2022-10-21 23:09:50 +0530

    added com.cg(parent)

---
+++ src/main/java/com/cg/FlightManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+
+public class FlightManagementSystemApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(FlightManagementSystemApplication.class,args);
+
+	}
+
+}

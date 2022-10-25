5bb9a7fc9ad2f3eb418b740ba79c14bab89b6d17
ishika0601 <ishikaa.v.singhal@gmail.com>
2022-10-25 16:24:56 +0530

    Added flight exceptions

--- src/main/java/com/cg/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/cg/advice/GlobalExceptionAdvice.java
@@ -5,11 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.cg.exception.AirportNotFoundException;
-import com.cg.exception.InvalidScheduledFlightException;
-import com.cg.exception.InvalidUserException;
-import com.cg.exception.ScheduledFlightNotFoundException;
-import com.cg.exception.UserNotFoundException;
+import com.cg.exception.*;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
@@ -40,6 +36,18 @@ public class GlobalExceptionAdvice {
 	@ExceptionHandler(InvalidScheduledFlightException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	String scheduledFlightInvalidHandler(InvalidScheduledFlightException e) {
+		return e.getMessage();
+	}
+	
+	@ExceptionHandler(FlightNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String flightNotFoundHandler(FlightNotFoundException e) {
+		return e.getMessage();
+	}
+	
+	@ExceptionHandler(InvalidFlightException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	String flightInvalidHandler(InvalidFlightException e) {
 		return e.getMessage();
 	}
 	

c96e122eee9c74bf5bfac2384249a7e9013c64df
simranmansuri <simranbmansuri@gmail.com>
2022-11-17 22:19:53 +0530

    commit

--- src/main/java/com/cg/controller/AirportController.java
+++ src/main/java/com/cg/controller/AirportController.java
@@ -36,7 +36,7 @@ public class AirportController {
 	METHOD : GET
 	 */
 	@GetMapping("/showById/{code}")
-	public Airport showById(@PathVariable BigInteger code) {
+	public Airport showById(@PathVariable BigInteger code){
 		return airportService.viewAirport(code);
 	}
 	

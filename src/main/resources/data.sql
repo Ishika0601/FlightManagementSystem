a67e6cb44796515d420ef045ff8cc6c1ca5fd488
ishika0601 <ishikaa.v.singhal@gmail.com>
2022-11-24 01:58:53 +0530

    data added in all entities

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,9 +14,20 @@ insert into airport(code,name,location) values (113,'Calicut International Airpo
 insert into airport(code,name,location) values (114,'Trivandrum International Airport','Thiruvananthapuram');
 insert into airport(code,name,location) values (115,'Sheikh ul-Aalam International Airport','Srinagar');
 
-insert into flight(fno, cname, fmodel, seatcap) values (10001,'Indigo', 'Jet', 100);
-insert into flight(fno, cname, fmodel, seatcap) values (10002,'Spicejet', 'Airbus', 120);
-insert into flight(fno, cname, fmodel, seatcap) values (10003,'Air India', 'Jet', 150);
+
+insert into flight(fno, cname, fmodel, seatcap) values (10001,'IndiGo', 'Airbus A220', 100);
+insert into flight(fno, cname, fmodel, seatcap) values (10002,'SpiceJet', 'ATR 42', 120);
+insert into flight(fno, cname, fmodel, seatcap) values (10003,'Air India', 'Boeing 757', 150);
+insert into flight(fno, cname, fmodel, seatcap) values (10004,'Akasa Air', 'Boeing 787', 180);
+insert into flight(fno, cname, fmodel, seatcap) values (10005,'Go First', 'Embraer 170', 200);
+insert into flight(fno, cname, fmodel, seatcap) values (10006,'IndiGo', 'Dornier 328', 130);
+insert into flight(fno, cname, fmodel, seatcap) values (10007,'Vistara', 'Boeing 787', 180);
+insert into flight(fno, cname, fmodel, seatcap) values (10008,'Jet Airways', 'Boeing 767', 100);
+insert into flight(fno, cname, fmodel, seatcap) values (10009,'Air India', 'Airbus A319', 190);
+insert into flight(fno, cname, fmodel, seatcap) values (10010,'SpiceJet', 'Embraer 170', 200);
+insert into flight(fno, cname, fmodel, seatcap) values (10011,'Go First', 'Boeing 757', 150);
+insert into flight(fno, cname, fmodel, seatcap) values (10012,'Jet Airways', 'Boeing 787', 180);
+
 
 insert into user(id, email, name, pwd, phno, type) values (11, 'hemant@gmail.com', 'Hemant', 'hemant@1129', 7738684418, 'Admin');
 insert into user(id, email, name, pwd, phno, type) values (12, 'sahil@gmail.com', 'Sahil', 'sahil@0307', 7032716763, 'Admin');
@@ -25,7 +36,7 @@ insert into user(id, email, name, pwd, phno, type) values (14, 'simran@gmail.com
 insert into user(id, email, name, pwd, phno, type) values (15, 'harsh@gmail.com', 'Harsh', 'Harsh@123', 9068967542, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (16, 'amit123@gmail.com', 'Amit', 'Amity@567', 7623568178, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (17, 'mansiii12@gmail.com', 'Mansi', 'Mansi06@', 9769616719, 'Customer');
-insert into user(id, email, name, pwd, phno, type) values (18, 'sakshii89@gmail.com', 'Sakshi', 'saxxi@12345', 9079167656, 'Customer');
+insert into user(id, email, name, pwd, phno, type) values (18, 'sakshii89@gmail.com', 'Sakshi', 'sakshi@12345', 9079167656, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (19, 'aakash654@gmail.com', 'Akash', 'Akash@321', 8765456776, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (20, 'yogeshY@gmail.com', 'Yogesh', 'Yogesh@987', 7654568976, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (21, 'sarveshTiwari@gmail.com', 'Sarvesh', 'sarvesh@tiwari3', 9079166543, 'Customer');
@@ -39,10 +50,65 @@ insert into user(id, email, name, pwd, phno, type) values (28, 'akashsky@gmail.c
 insert into user(id, email, name, pwd, phno, type) values (29, 'parshant@gmail.com', 'Parshant', 'Parshant9065@', 9206763452, 'Customer');
 insert into user(id, email, name, pwd, phno, type) values (30, 'ashuSingh1305@gmail.com', 'Ashish', 'Ashu@1287', 9876512456, 'Customer');
 
-insert into schedule(sid, atime, dtime, destination_code, source_code) values (100,'2022-12-26 11:00:00','2022-12-26 09:00:00',109,101);
 
-insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (100,90,10001,100);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (101,'2022-12-25 17:50:00','2022-12-26 14:00:00',111,115);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (102,'2022-12-27 09:45:00','2022-12-27 07:00:00',112,101);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (103,'2022-12-26 19:00:00','2022-12-26 16:00:00',108,105);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (104,'2022-12-26 11:00:00','2022-12-26 09:00:00',109,101);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (105,'2022-12-25 20:00:00','2022-12-25 18:00:00',103,102);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (106,'2022-12-28 11:00:00','2022-12-28 09:00:00',110,109);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (107,'2022-12-27 18:50:00','2022-12-27 16:10:00',104,103);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (108,'2022-12-28 22:40:00','2022-12-28 19:00:00',110,114);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (109,'2022-12-26 11:40:00','2022-12-26 08:00:00',104,107);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (110,'2022-12-27 18:20:00','2022-12-27 16:20:00',101,105);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (111,'2022-12-28 19:50:00','2022-12-28 17:00:00',107,106);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (112,'2022-12-26 18:00:00','2022-12-26 15:00:00',102,101);
+insert into schedule(sid, atime, dtime, destination_code, source_code) values (113,'2022-12-25 13:50:00','2022-12-25 10:30:00',103,106);
 
-insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1000, '2022-12-24 12:00:00', 1, 6000, 100, 12);
 
-insert into passenger(pnr, luggage, age, name, uin, booking_id) values (1, 12, 22, 'Sahil', 836484251673, 1000);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (101, 96, 10001, 110);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (102, 200, 10005, 113);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (103, 149, 10011, 102);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (104, 119, 10002, 101);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (105, 148, 10003, 111);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (106, 118, 10002, 112);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (107, 200, 10005, 104);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (108, 97, 10008, 103);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (109, 100, 10008, 109);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (110, 128, 10006, 106);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (111, 180, 10007, 105);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (112, 190, 10009, 107);
+insert into scheduled_flight(sfid, avalseat, flight_fno, schedule_sid) values (113, 198, 10010, 108);
+
+
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1001, '2022-12-25 12:00:00', 2, 12000, 101, 11);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1002, '2022-12-24 12:00:00', 1, 6000, 113, 13);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1003, '2022-12-25 11:00:00', 1, 18000, 103, 15);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1004, '2022-12-25 12:00:00', 2, 12000, 105, 12);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1005, '2022-12-24 22:00:00', 1, 6000, 113, 12);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1006, '2022-12-25 10:00:00', 2, 12000, 101, 15);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1007, '2022-12-25 13:00:00', 1, 18000, 104, 11);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1008, '2022-12-24 12:00:00', 2, 12000, 106, 14);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1009, '2022-12-24 12:00:00', 1, 6000, 108, 17);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1010, '2022-12-25 12:00:00', 1, 18000, 110, 13);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1011, '2022-12-25 10:00:00', 1, 6000, 110, 14);
+insert into booking(booking_id, date, nop, cost, sfid, user_id) values (1012, '2022-12-24 12:00:00', 2, 12000, 108, 11);
+
+
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (1, 12, 22, 'Sahil', 836484251673, 1001);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (2, 1, 19, 'Harsh', 906896754245, 1001);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (3, 12, 34, 'Amit', 877623568178, 1002);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (4, 8, 23, 'Mansi', 976961671976, 1003);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (5, 3, 45, 'Sakshi', 907916765346, 1004);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (6, 0, 23 'Akash', 876545677566, 1004);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (7, 2, 46, 'Yogesh', 765454568976, 1005);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (8, 4, 24, 'Sarvesh', 879079166543, 1006);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (9, 6, 36, 'Palak', 339076512456, 1006);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (10, 2, 34, 'Janvi', 845978675406, 1007);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (11, 6, 23, 'Namrata', 965654554676, 1008);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (12, 2, 22, 'Ishika', 945234342206, 1008);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (13, 5, 22, 'Dhiraj', 439779162206, 1009);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (14, 11, 23, 'Hritik', 899765432456, 1010);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (15, 0, 14, 'Akash', 899564322206, 1011);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (16, 3, 35, 'Parshant', 899206763452, 1012);
+insert into passenger(pnr, luggage, age, name, uin, booking_id) values (17, 1, 20, 'Ashish', 989876512456, 1012);
